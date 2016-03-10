3b3b8993e4e1748b6b40d553c7707e9ace01ef57
davidogle <the_davester_808@hotmail.com>
2016-03-10 14:57:22 +0000

    convert to pixels given a static dpi

--- GetSearchWindowSize.m
+++ GetSearchWindowSize.m
@@ -1,12 +1,15 @@
 function [ ssize ] = GetSearchWindowSize( f, z, b )
 %GetSearchWindowSize a function to calculate the optimal search window size
-%f = focul length, 0.05 = 50mm
-%z = min depth of object 1 = 1M, 
+%f = focul length, 50mm
+%z = min depth of object 1000 = 1 meter, 
 x = 1; %any value of x will produce the same result
-%b = baseline, 0.3 = 30cm
+%b = baseline, 300 = 30cm
+dpi =72;
+
 
 xr = (f / z) * (x-b);
 xl = (f / z) * x;
-ssize = abs(xl - xr) * 2;
+mm = abs(xl - xr) * 2;
+ssize = (mm * dpi) / 25.4;
 end
 
