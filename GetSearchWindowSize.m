986a20c1d398b8a9cb1b5e92b1dc06cd948cba14
davidogle <the_davester_808@hotmail.com>
2016-03-11 13:34:55 +0000

    optimize

--- GetSearchWindowSize.m
+++ GetSearchWindowSize.m
@@ -2,14 +2,11 @@ function [ ssize ] = GetSearchWindowSize( f, z, b )
 %GetSearchWindowSize a function to calculate the optimal search window size
 %f = focul length, 50mm
 %z = min depth of object 1000 = 1 meter, 
-x = 1; %any value of x will produce the same result
 %b = baseline, 300 = 30cm
 dpi =72;
 
-
-xr = (f / z) * (x-b);
-xl = (f / z) * x;
-mm = abs(xl - xr) * 2;
+xr = (f / z) * (-b);
+mm = abs(xr) * 2;
 ssize = (mm * dpi) / 25.4;
 end
 
