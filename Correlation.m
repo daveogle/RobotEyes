1a08b8c5707fb89171c3d7ffb163f2db2f17fd0c
davidogle <the_davester_808@hotmail.com>
2016-03-20 21:52:29 +0000

    Update epipolar function to return top N corner matching pairs.

--- Correlation.m
+++ Correlation.m
@@ -1,15 +1,18 @@
 function [ result ] = Correlation( image1, image2, originPoint, searchPoints, supportSize )
 
-[searchBoundsX, searchBoundsY] = size(searchPoints);
+[searchBoundsX,] = size(searchPoints);
 halfSupport = floor(supportSize /2);
+pImage1 = padarray(image1, [halfSupport halfSupport], 0, 'both');
+pImage2 = padarray(image2, [halfSupport halfSupport], 0, 'both');
 
-minDiff = 9999;
+minDiff = -1;
 result = zeros(1,3);
 
     for x = 1 : searchBoundsX
-       diff = SUPPORT_CMP(image1, image2, originPoint, searchPoints(x), halfSupport);
-       if diff < minDiff
-        result = [searchPoints(x), diff];
+       diff = SUPPORT_CMP(pImage1, pImage2, (originPoint + halfSupport), (searchPoints(x,:) + halfSupport), halfSupport);
+       if minDiff < 0 || diff < minDiff
+        minDiff = diff;
+        result = [searchPoints(x,:), diff];
        end
     end
 end
