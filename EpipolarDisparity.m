086b342464b512b212d330fbafbf658d06e0fc5d
davidogle <the_davester_808@hotmail.com>
2016-03-22 18:00:00 +0000

    epipolar working

--- EpipolarDisparity.m
+++ EpipolarDisparity.m
@@ -2,15 +2,13 @@ function [ map ] = EpipolarDisparity( image1, image2, corners, support_size, sea
 
 halfSupport = floor(support_size /2);
 halfSearch = floor(searchWinSize /2);
+[sizeX, sizeY] = size(image1);
 %compute fundemental matrix
 [matchedPoints1, matchedPoints2] = cornerCorrelation(image1, image2, corners, support_size, searchWinSize);
 m1 = matchedPoints1.Location;
 m2 = matchedPoints2.Location;
-%matchedPoints1 = matchedPoints(:,1 : 2);
-%matchedPoints2 = matchedPoints(:,3 : 4);
-
+                 
 %CODE TO SHOW THE EPIPOLAR LINES
-%[fLMedS, inliers]
 [F, inliers, status] = estimateFundamentalMatrix(matchedPoints1, matchedPoints2, 'Method', 'LMedS');
 if status ~=0
 1
@@ -24,66 +22,106 @@ showMatchedFeatures(image1, image2, inlierPoints1, inlierPoints2);
 legend('Inlier points in I1', 'Inlier points in I2');
 
 %rectify image
-[T1,T2] = estimateUncalibratedRectification(F, inlierPoints1.Location, inlierPoints2.Location, size(image2));
-tform1 = projective2d(T1);
-tform2 = projective2d(T2);
-[J1,J2] = rectifyStereoImages(image1, image2, tform1, tform2, 'OutputView','valid');
-resultLeft = zeros(size(J1));
-resultRight = zeros(size(J1));
+% [T1,T2] = estimateUncalibratedRectification(F, inlierPoints1.Location, inlierPoints2.Location, size(image2));
+% tform1 = projective2d(T1);
+% tform2 = projective2d(T2);
+% [J1,J2] = rectifyStereoImages(image1, image2, tform1, tform2, 'OutputView','valid');
+% resultLeft = zeros(size(J1));
+% resultRight = zeros(size(J1));
 
-%Show charts
-figure;
-imshow(stereoAnaglyph(J1,J2));
-
-figure;
-showMatchedFeatures(image1,image2,matchedPoints1,matchedPoints2,'montage','PlotOptions',{'ro','go','y--'});
-title('Putative point matches');
-
-figure;
-subplot(121); imshow(image1);
-title('Matched points and Epipolar Lines in First Image'); hold on;
-plot(m1(inliers,1), m1(inliers,2), 'go')
-epiLines = epipolarLine(F', m2(inliers, :));
+%get line coords
+%get image co-ords
+coords = getImageCoords(image1);
+%find epipolar lines
+epiLines = epipolarLine(F, coords);
 points = lineToBorderPoints(epiLines, size(image1));
-line(points(:, [1,3])', points(:, [2,4])');
+results = zeros(sizeX,sizeY);
+%Show charts
+% figure;
+% imshow(stereoAnaglyph(J1,J2));
 
-subplot(122); imshow(image2);
-title('Matched points and Epipolar Lines in Second Image'); hold on;
-plot(m2(inliers,1), m2(inliers,2), 'go')
-epiLines = epipolarLine(F, m1(inliers, :));
-points = lineToBorderPoints(epiLines, size(image2));
-line(points(:, [1,3])', points(:, [2,4])');
-truesize;
+% figure;
+% showMatchedFeatures(image1,image2,matchedPoints1,matchedPoints2,'montage','PlotOptions',{'ro','go','y--'});
+% title('Putative point matches');
+% 
+% figure;
+% subplot(121); imshow(image1);
+% title('Matched points and Epipolar Lines in First Image'); hold on;
+% plot(m1(inliers,1), m1(inliers,2), 'go')
+% epiLines = epipolarLine(F', m2(inliers, :));
+% points = lineToBorderPoints(epiLines, size(image1));
+% line(points(:, [1,3])', points(:, [2,4])');
+% 
+% subplot(122); imshow(image2);
+% title('Matched points and Epipolar Lines in Second Image'); hold on;
+% plot(m2(inliers,1), m2(inliers,2), 'go')
+% epiLines = epipolarLine(F, m1(inliers, :));
+% points = lineToBorderPoints(epiLines, size(image2));
+% line(points(:, [1,3])', points(:, [2,4])');
+% truesize;
 
-[imageRow, imageCol] = size(J1);
 %pad images for comparison
-pImage1 = padarray(J1, [halfSupport halfSupport], 0, 'both');
-pImage2 = padarray(J2, [halfSupport halfSupport], 0, 'both');
+pImage1 = padarray(image1, [halfSupport halfSupport], 0, 'both');
+pImage2 = padarray(image2, [halfSupport halfSupport], 0, 'both');
+[psizex, psizey] = size(pImage1);
 
-for row = 1 : imageRow
-    
-    for col = 1 : imageCol
-        best = [0,0,-1];
-        %set search window bounds
-        minW = col - halfSearch;
-        maxW = col + halfSearch;
-        if minW < 1
-            minW = 1;
-        end
-        if maxW > imageCol
-            maxW = imageCol;
-        end
-        for x = minW : maxW
-            cost = SUPPORT_CMP(pImage1, pImage2, [row, col] + halfSupport, [row,x] + halfSupport, halfSupport);
-            if best(3) == -1 || cost < best(3)
-                best = [col,x, cost];
+for epLine = 1 : length(points);
+    best = [0,0,-1];
+    lineCoords = bresenham(points(epLine, 1), points(epLine, 2), points(epLine, 3), points(epLine, 4));
+    if lineCoords(1) ~= -1
+        for j = 1 : length(lineCoords)
+            try
+                if lineCoords(j,1) <= sizeY && lineCoords(j,2) <= sizeX ...
+                   && lineCoords(j,1) <= coords(epLine,1) + halfSearch && lineCoords(j,2) <= coords(epLine,2) + halfSearch ...
+                   && lineCoords(j,1) >= coords(epLine,1) - halfSearch && lineCoords(j,2) >= coords(epLine,2) - halfSearch
+                    cost = SUPPORT_CMP(pImage1, pImage2, [coords(epLine,2),coords(epLine,1)] + halfSupport, [lineCoords(j,2),lineCoords(j,1)] + halfSupport, halfSupport);
+                     if best(3) == -1 || cost < best(3)
+                         best = [lineCoords(j,1),lineCoords(j,2), cost];
+                     end
+                end
+            catch E
+                3
             end
         end
-        resultLeft(row,col) = best(1);
-        resultRight(row,col) = best(2);
     end
+    resultLeft = coords(epLine,:);
+    resultRight = [best(1), best(2)];
+    temp = resultLeft - resultRight;
+    results(coords(epLine, 2), coords(epLine, 1)) = sqrt(temp(1)^2 + temp(2)^2);
 end
-map = resultLeft - resultRight;
+
+% for row = 1 : imageRow
+%     minYW = row - halfSearch;
+%     maxYW = row + halfSearch;
+%     if minYW < 1
+%         minYW = 1;
+%     end
+%     if maxYW > imageRow
+%         maxYW = imageRow;
+%     end
+%     for col = 1 : imageCol
+%         best = [0,0,-1];
+%         %set search window bounds
+%         minXW = col - halfSearch;
+%         maxXW = col + halfSearch;
+%         if minXW < 1
+%             minXW = 1;
+%         end
+%         if maxXW > imageCol
+%             maxXW = imageCol;
+%         end
+%         for linePointElement = 1 : length(linePoints)
+%             cost = SUPPORT_CMP(pImage1, pImage2, [row, col] + halfSupport, [row,x] + halfSupport, halfSupport);
+%             if best(3) == -1 || cost < best(3)
+%                 best = [col,x, cost];
+%             end
+%         end
+%         resultLeft(row,col) = best(1);
+%         resultRight(row,col) = best(2);
+%     end
+% end
+%map = resultLeft - resultRight;
+map = results;
 maxV = max(max(map));
 minV = min(min(map));
 figure;
@@ -127,16 +165,16 @@ end
 % end
 % end
 %
-% function [coords] = getImageCoords(image)
-%     [rowMax, colMax] = size(image);
-%     coords = zeros((rowMax * colMax), 2);
-%     coordsNumber = 1;
-%     for row = 1 : rowMax
-%         for col = 1 : colMax
-%             coords(coordsNumber,:) = [col, row];
-%             coordsNumber = coordsNumber + 1;
-%         end
-%     end
-% end
+function [coords] = getImageCoords(image)
+    [rowMax, colMax] = size(image);
+    coords = zeros((rowMax * colMax), 2);
+    coordsNumber = 1;
+    for row = 1 : rowMax
+        for col = 1 : colMax
+            coords(coordsNumber,:) = [col, row];
+            coordsNumber = coordsNumber + 1;
+        end
+    end
+end
 
 
--- SUPPORT_CMP.m
+++ SUPPORT_CMP.m
@@ -1,7 +1,11 @@
 function [ diff ] = SUPPORT_CMP(image1, image2, location1, location2, wsize)
-    w1 = image1((location1(1) - wsize):(location1(1) + wsize), (location1(2) - wsize):(location1(2) + wsize));
-    w2 = image2((location2(1) - wsize):(location2(1) + wsize), (location2(2) - wsize):(location2(2) + wsize));
-    w1 =double(w1);
-    w2 = double(w2);
-    diff = sum( (w1(:) - w2(:)).^2 );
+    try
+        w1 = image1((location1(1) - wsize):(location1(1) + wsize), (location1(2) - wsize):(location1(2) + wsize));
+        w2 = image2((location2(1) - wsize):(location2(1) + wsize), (location2(2) - wsize):(location2(2) + wsize));
+        w1 =double(w1);
+        w2 = double(w2);
+        diff = sum( (w1(:) - w2(:)).^2 );
+    catch E
+        3
+    end
 end
